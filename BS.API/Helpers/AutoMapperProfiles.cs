2339b85ba093c94c7d5b0ad4d2bccf60f255b95b
vladosfi <sfi@abv.bg>
2021-02-02 20:03:48 +0200

    Apply automapper

--- BS.API/Helpers/AutoMapperProfiles.cs
+++ BS.API/Helpers/AutoMapperProfiles.cs
@@ -10,10 +10,15 @@ namespace BS.API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<User, UserForListDto>()
-                .ForMember(dest => dest.PhotoUrl, opt => 
-                    opt.MapFrom(src => src.Photos.FirstOrDefault(p=>p.IsMain).Url));
-            CreateMap<User, UserForDetailedDto>().ForMember(dest => dest.PhotoUrl, opt => 
-                    opt.MapFrom(src => src.Photos.FirstOrDefault(p=>p.IsMain).Url));
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt =>
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt =>
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
