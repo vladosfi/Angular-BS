616945fb81d98d4bbd1968fd0e1df0009ba71f69
vladosfi <sfi@abv.bg>
2021-02-04 09:22:55 +0200

    Add edit member component

--- BSApp-SPA/src/app/app.module.ts
+++ BSApp-SPA/src/app/app.module.ts
@@ -19,9 +19,11 @@ import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { NgxGalleryModule } from 'ngx-gallery-9';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export function tokenGetter(){
   return localStorage.getItem('token');
@@ -38,6 +40,7 @@ export function tokenGetter(){
       MessagesComponent,
       MemberCardComponent,
       MemberDetailComponent,
+      MemberEditComponent,
    ],
   imports: [
     BrowserModule,
@@ -59,7 +62,8 @@ export function tokenGetter(){
   providers: [
     ErrorInterceptorProvider,
     MemberDetailResolver,
-    MemberListResolver
+    MemberListResolver,
+    MemberEditResolver,
   ],
   bootstrap: [AppComponent]
 })
--- BSApp-SPA/src/app/routes.ts
+++ BSApp-SPA/src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { ListsComponent } from './lists/lists.component'
 import { MemberDetailComponent } from './members/member-detail/member-detail.component'
+import { MemberEditComponent } from './members/member-edit/member-edit.component'
 import { MemberListComponent } from './members/member-list/member-list.component'
 import { MessagesComponent } from './messages/messages.component'
 import { AuthGuard } from './_guards/auth.guard'
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver'
+import { MemberEditResolver } from './_resolvers/member-edit.resolver'
 import { MemberListResolver } from './_resolvers/member-list.resolver'
 
 export const appRoutes: Routes = [
@@ -23,6 +25,8 @@ export const appRoutes: Routes = [
                 path: 'members/:id', component: MemberDetailComponent,
                 resolve: { user: MemberDetailResolver }
             },
+            { path: 'member/edit', component: MemberEditComponent ,
+            resolve: { user: MemberEditResolver }},
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent },
         ]
